aa77cfb7c54baa5711285a35ffaea00795b116cc
Riccardo Viviano <riciric83@gmail.com>
2018-02-28 21:33:28 +0100

    Update stochastic_gradient_descent.c

--- C/cpu/lib/SGD/stochastic_gradient_descent.c
+++ C/cpu/lib/SGD/stochastic_gradient_descent.c
@@ -14,8 +14,7 @@
  * 		@double** d_w: partial derivative of the weights
  * 		@double* d_b: partial derivative of the biases
  * 		@int rows: rows of w and d_w
- * 		@int cols: number of columns of w and d_w
- * 		@int len: length of b and d_b
+ * 		@int cols: number of columns of w and d_b
  * Output:
  * 		@int 0: error doesn't occur
  * */
@@ -23,8 +22,8 @@
 
 int
 SGD (int mini_batch_size, double **weights, double *biases,
-     double learning_rate, double **d_w, double *d_b, int rows, int cols,
-     int len)
+     double learning_rate, double **d_w, double *d_b, int rows, int cols
+     )
 {
   int i, j, k;
   for (i = 0; i < layers; i++)
